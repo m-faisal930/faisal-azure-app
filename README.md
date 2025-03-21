cf66b097e2592f27c977cd4735c581c4179cab5d
m-faisal930 <faisal.mr333@gmail.com>
2025-03-21 10:53:28 +0500

    added project files

---
+++ README.md
@@ -0,0 +1,2 @@
+# Portfolio Web App
+This is a personal portfolio website showcasing my skills, projects, and contact information.
---
+++ app.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+require('dotenv').config();
+
+
+app.get('/', (req, res) => {
+  res.send(`<h1>Welcome, ${process.env.USERNAME}</h1>`);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Serve static files (CSS, images, etc.)
+app.use(express.static('public'));
+
+// Routes
+app.get('/home', (req, res) => {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+app.get('/about', (req, res) => {
+  res.sendFile(__dirname + '/views/about.html');
+});
+
+app.get('/projects', (req, res) => {
+  res.sendFile(__dirname + '/views/projects.html');
+});
+
+app.get('/contact', (req, res) => {
+  res.sendFile(__dirname + '/views/contact.html');
+});
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
